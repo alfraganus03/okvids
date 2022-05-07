7caa20b19abea79add3c8d566a67c3fd25a6b98b
alfraganus03 <104312275+alfraganus03@users.noreply.github.com>
2022-05-07 21:49:24 +0500

    Update localisation.py

--- LOCAL/localisation.py
+++ LOCAL/localisation.py
@@ -1,15 +1,15 @@
 #Don't be a thief by stealing other's Hardwork, it took time and effort to make this repo.
 #Respect my work by not making any changes here.
 
-START_TEXT = "Send me any file to begin."
+START_TEXT = "Video yuboring"
 
-FORCE_SUB_TEXT = "In order to use this bot, you've to join my parent channel."
+FORCE_SUB_TEXT = "Botni ishlatish uchun kanalga obina bo'ling"
 
-CHANNEL_LINK = "https://t.me/DroneBots"
+CHANNEL_LINK = "https://t.me/Azizbek_03"
 
-SUPPORT_LINK = "https://t.me/TeamDrone"
+SUPPORT_LINK = "https://t.me/lfraganus"
 
-info_text = "This bot is developed by @MaheshChauhan\n\nWritten in python library TELETHON.\n\nBot by : @DroneBots\nSupport : @TeamDrone\n\nV1.4"   
+info_text = "Bot hali chala"   
 
 help_text = """**v1.4**
 
@@ -27,11 +27,11 @@ help_text = """**v1.4**
 
 •`Trim` - cut your videos"""
 
-source_text = "**Deploy your own bot**"
+source_text = "**Test**"
 
-DEV = "https://t.me/MaheshChauhan"
+DEV = "https://t.me/lfraganus"
 
-spam_notice = "This bot is hosted on heroku, and hence can just run one process at a time.Spamming the bot or encoding adult videos will lead you to a ban."
+spam_notice = "Spamga ban :)"
 
 JPG = "LOCAL/video_convertor.jpg"
 
